8900ebe9525c51f3ca3d1aa357d2a504d812fca6
ma15569 <ma15569@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2020-06-10 11:04:49 +0000

    Added Extension verion number and name

--- plug-ins/ojmapcoloring/src/main/java/org/freevoice/mapcoloring/FiveColorExtension.java
+++ plug-ins/ojmapcoloring/src/main/java/org/freevoice/mapcoloring/FiveColorExtension.java
@@ -1,27 +1,28 @@
 /*
-* 
-*  The JUMP DB Query Plugin is Copyright (C) 2007  Larry Reeder
-*  JUMP is Copyright (C) 2003 Vivid Solutions
-* 
-*  This file is part of the JUMP DB Query Plugin.
-*  
-*  The JUMP DB Query Plugin is free software; you can redistribute it and/or 
-*  modify it under the terms of the Lesser GNU General Public License as 
-*  published *  by the Free Software Foundation; either version 3 of the 
-*  License, or  (at your option) any later version.
-*  
-*  This software is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  Lesser GNU General Public License for more details.
-*  
-*  You should have received a copy of the GNU General Public License
-*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * 
+ *  The JUMP DB Query Plugin is Copyright (C) 2007  Larry Reeder
+ *  JUMP is Copyright (C) 2003 Vivid Solutions
+ * 
+ *  This file is part of the JUMP DB Query Plugin.
+ *  
+ *  The JUMP DB Query Plugin is free software; you can redistribute it and/or 
+ *  modify it under the terms of the Lesser GNU General Public License as 
+ *  published *  by the Free Software Foundation; either version 3 of the 
+ *  License, or  (at your option) any later version.
+ *  
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  Lesser GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.freevoice.mapcoloring;
 
-import com.vividsolutions.jump.workbench.plugin.Extension;
 import com.vividsolutions.jump.workbench.plugin.PlugInContext;
+
+import sun.security.x509.Extension;
 
 /**
  * OJ extension class for map coloring plugin
@@ -29,8 +30,17 @@ import com.vividsolutions.jump.workbench.plugin.PlugInContext;
 public class FiveColorExtension extends Extension
 {
 
-    public void configure(PlugInContext context) throws Exception
-    {
-        new FiveColorPlugin().initialize(context);
-    }
+	public void configure(PlugInContext context) throws Exception
+	{
+		new FiveColorPlugin().initialize(context);
+	}
+	@Override
+	public String getVersion() {
+		return "0.5.2 (2020-06-10)";
+	}
+
+	@Override
+	public String getName() {
+		return "Map Coloring (Five color theorem - https://en.wikipedia.org/wiki/Five_color_theorem)";
+	}
 }
