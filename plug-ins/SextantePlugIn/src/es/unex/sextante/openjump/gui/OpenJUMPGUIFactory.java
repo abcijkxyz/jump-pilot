434cc5c9b73e2e21e9698d4b05e02911e11ecd2b
ma15569 <ma15569@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2020-07-07 14:04:29 +0000

    Sextante Toolbox now will open as DetachedInternalFrame in OpenJUMP

--- plug-ins/SextantePlugIn/src/es/unex/sextante/openjump/gui/OpenJUMPGUIFactory.java
+++ plug-ins/SextantePlugIn/src/es/unex/sextante/openjump/gui/OpenJUMPGUIFactory.java
@@ -1,22 +1,51 @@
 package es.unex.sextante.openjump.gui;
 
 import javax.swing.JDialog;
+import javax.swing.JInternalFrame;
 import javax.swing.JOptionPane;
+
+import com.vividsolutions.jump.workbench.JUMPWorkbench;
+import com.vividsolutions.jump.workbench.ui.WorkbenchFrame;
 
 import es.unex.sextante.core.GeoAlgorithm;
 import es.unex.sextante.gui.core.DefaultGUIFactory;
+import es.unex.sextante.gui.core.SextanteGUI;
+import es.unex.sextante.openjump.toolbox.ToolboxFrame;
 
 public class OpenJUMPGUIFactory
-         extends
-            DefaultGUIFactory {
+extends
+DefaultGUIFactory {
 
-   @Override
-   public void showBatchProcessingDialog(final GeoAlgorithm alg,
-                                         final JDialog parent) {
+	@Override
+	public void showBatchProcessingDialog(final GeoAlgorithm alg,
+			final JDialog parent) {
 
-      JOptionPane.showMessageDialog(parent, "Batch processing not yet implemented");
+		JOptionPane.showMessageDialog(parent, "Batch processing not yet implemented");
 
-   }
+	}
+
+
+
+
+	@Override
+	public void showToolBoxDialog() {
+		WorkbenchFrame wFrame = JUMPWorkbench.getInstance().getFrame();
+		SextanteGUI.getInputFactory().createDataObjects();
+
+		for (JInternalFrame iFrame : wFrame
+				.getInternalFrames()) {
+			if (iFrame instanceof ToolboxFrame) {
+
+				iFrame.toFront();
+				return;
+
+			}
+		}
+		ToolboxFrame additionalResultsFrame = new ToolboxFrame(wFrame);
+
+		wFrame
+		.addInternalFrame(additionalResultsFrame);
+	} 
 
 
 }
